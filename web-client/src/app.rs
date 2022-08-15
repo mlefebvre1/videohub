16b0eaebb8f780d6483eba2fa371360d6c8bda76
Michael <michael.lefebvre@riedel.net>
2022-08-15 12:57:32 -0400

    wip web-client

--- web-client/src/app.rs
+++ web-client/src/app.rs
@@ -1,14 +1,14 @@
 use std::net::Ipv4Addr;
 
-use crate::route;
-use log::warn;
-use videohub::{protocol::HubInfo, Hub, HubError, DEFAULT_DEVICE_PORT};
-use wasm_bindgen::UnwrapThrowExt;
+use reqwest::Client;
+use videohub::protocol::{DeviceInfo, OutputRoutings};
+
 use yew::prelude::*;
 
 pub struct Model {
     nb_input_ports: Option<usize>,
     nb_output_ports: Option<usize>,
+    msg: String,
 }
 
 #[derive(Properties, PartialEq)]
@@ -16,11 +16,30 @@ pub struct Props {
     pub ipv4_addr: Ipv4Addr,
 }
 
+pub enum Msg {
+    FetchDeviceInfo(DeviceInfo),
+    FetchText(String),
+    FetchOutputRoutings(OutputRoutings),
+}
+
 impl Component for Model {
-    type Message = ();
+    type Message = Msg;
     type Properties = Props;
 
     fn create(ctx: &Context<Self>) -> Self {
+        ctx.link().send_future(async {
+            let client = Client::new();
+            let s = client
+                .get("http://192.168.1.102:8000/device_info")
+                .header(reqwest::header::CONTENT_TYPE, "application/json")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+            Msg::FetchText(s)
+        });
         // ctx.link().send_future(async {
         //     let hub_info = get_hub_info(ctx.props().ipv4_addr).await?;
         // });
@@ -36,20 +55,37 @@ impl Component for Model {
         //     nb_output_ports: Some(hub_info.device_info.nb_video_outputs),
         // }
         Self {
-            nb_input_ports: Some(1),
-            nb_output_ports: Some(1),
+            nb_input_ports: Some(0),
+            nb_output_ports: Some(0),
+            msg: "".to_owned(),
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::FetchDeviceInfo(device_info) => {
+                self.nb_input_ports = Some(device_info.nb_video_inputs);
+                self.nb_output_ports = Some(device_info.nb_video_outputs);
+                true
+            }
+            Msg::FetchText(s) => {
+                self.msg = s;
+                true
+            }
+            Msg::FetchOutputRoutings(_output_routings) => {
+                //todo
+                false
+            }
+        }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <>
                 <p>{"Test"}</p>
-                <p>{format!("{}", _ctx.props().ipv4_addr)}</p>
+                <p>{format!("Some message: {}", self.msg)}</p>
+                <p>{format!("NB INPUT PORTS : {}", self.nb_input_ports.unwrap())}</p>
+                <p>{format!("NB OUTPUT PORTS : {}", self.nb_output_ports.unwrap())}</p>
             </>
         }
 
